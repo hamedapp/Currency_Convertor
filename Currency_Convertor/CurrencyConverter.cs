ca0ff32e0ed955378a3e018e1fb15c32fc2fbc74
Hamed Alinia <hamedalinia7@gmail.com>
2022-07-18 11:07:35 +0430

    Add project files.

---
+++ Currency_Convertor/CurrencyConverter.cs
@@ -0,0 +1,97 @@
+﻿using QuickGraph;
+using QuickGraph.Algorithms;
+using System;
+using System.Collections.Generic;
+
+namespace Currency_Convertor
+{
+    public class CurrencyConverter: ICurrencyConverter
+    {
+        private static readonly CurrencyConverter instance = new CurrencyConverter();
+
+        private CurrencyConverter()
+        {
+        }
+        public static CurrencyConverter Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private readonly AdjacencyGraph<string, Edge<string>> _graph = new AdjacencyGraph<string, Edge<string>>();
+        private readonly Dictionary<Edge<string>, double> _costs = new Dictionary<Edge<string>, double>();
+
+        private void AddEdgeWithCosts(string source, string target, double cost)
+        {
+            var edge = new Edge<string>(source, target);
+
+            _graph.AddVerticesAndEdge(edge);
+            _costs.Add(edge, cost);
+        }
+
+        public void ClearConfiguration()
+        {
+            _graph.Clear();
+            _costs.Clear();
+        }
+
+        public double Convert(string fromCurrency, string toCurrency, double amount)
+        {
+            CheckPathFromGraph(fromCurrency, toCurrency, out bool isPathExist, out IEnumerable<Edge<string>> path);
+
+            if (isPathExist)
+            {
+                return CalculateAmountFromGraph(amount, path); ;
+            }
+            else
+            {
+                Console.WriteLine("No path found from {0} to {1}.");
+                return -1;
+            }
+        }
+
+        private void CheckPathFromGraph(string fromCurrency, string toCurrency, out bool isPathExist, out IEnumerable<Edge<string>> path)
+        {
+            var edgeCost = AlgorithmExtensions.GetIndexer(_costs);
+            var tryGetPath = _graph.ShortestPathsDijkstra(edgeCost, fromCurrency);
+
+            isPathExist = tryGetPath(toCurrency, out path);
+        }
+
+        private double CalculateAmountFromGraph(double amount, IEnumerable<Edge<string>> path)
+        {
+            double finalAmount = amount;
+
+            foreach (var item in path)
+            {
+                var cost = _costs[item];
+                finalAmount *= cost;
+            }
+
+            return finalAmount;
+        }
+
+        public void UpdateConfiguration(IEnumerable<ValueTuple<string, string, double>> conversionRates)
+        {
+            foreach (var item in conversionRates)
+            {
+                bool rateExist = _graph.TryGetEdge(item.Item1, item.Item2, out Edge<string> prevEdge);
+
+                if (rateExist)
+                {
+                    _graph.RemoveEdge(prevEdge);
+
+                    AddEdgeWithCosts(item.Item1, item.Item2, item.Item3);
+
+                    _costs[prevEdge] = item.Item3;
+
+                    continue;
+                }
+
+                AddEdgeWithCosts(item.Item1, item.Item2, item.Item3);
+            }
+        }
+    }
+}
---
+++ Currency_Convertor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Currency_Convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currenctConverItems = new List<ValueTuple<string, string, double>>
+            {
+                new ValueTuple<string, string, double>("USD", "CAD", 1.34),
+                new ValueTuple<string, string, double>("CAD", "GBP", 0.58),
+                new ValueTuple<string, string, double>("USD", "EUR", 0.86),
+                new ValueTuple<string, string, double>("USD", "CAD", 1.35),
+                new ValueTuple<string, string, double>("EUR", "IRR", 3.86),
+            };
+
+            CurrencyConverter convertor = CurrencyConverter.Instance;
+            convertor.UpdateConfiguration(currenctConverItems);
+
+            foreach (var item in currenctConverItems)
+            {
+                Console.WriteLine($"Add {item.Item1} to {item.Item2} with rate {item.Item3} to currency convert graph");
+            }
+
+            var finalAmount = convertor.Convert("USD", "IRR", 1000);
+
+            Console.WriteLine($"Converted Value from USD to IRR is {finalAmount}");
+            convertor.ClearConfiguration();
+
+            Console.ReadLine();
+        }
+    }
+}
